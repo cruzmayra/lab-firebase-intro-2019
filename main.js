f42c7110c42257e14198d1e72e20db5d9428bcc3
cruzmayra <you@example.com>
2019-02-15 20:20:54 -0600

    primer clase

--- main.js
+++ main.js
@@ -13,49 +13,49 @@ let user
 
 // FIREBASE INITIALIZE AND REFS
 
-// let firestore = firebase.firestore()
-// let memesRef = firestore.collection('memes')
+let firestore = firebase.firestore()
+let memesRef = firestore.collection('memes')
 
 
 //FIREBASE AUX FUNCTIONS
 
-// function writeMeme(object){
-//     let id = memesRef.doc().id
-//     memesRef.doc(id).set(object)
-// }
+function writeMeme(object){
+  let id = memesRef.doc().id
+  object.id = id
+  memesRef.doc(id).set(object)
+}
 
 // consultas
 
-// function searchFor(string){
-//     console.log("buscando", string)
-//     memesRef.where('tags', "==", string)
-//     .get()
-//     .then(function(snap){
-//         if(snap.empty) alert("Sin resultados") 
-//         snap.forEach(doc=>{
-//             resultsContainer.innerHTML = ""
-//             let figure = document.createElement('figure')
-//             figure.innerHTML = `
-//                 <img src="${doc.data().link}" alt="${doc.data().title}" />
-//             `
-//             resultsContainer.appendChild(figure)
-//         })
-//     })
-// }
+function searchFor(string){
+     console.log("buscando", string)
+     memesRef.where('tags', "==", string)
+     .get()
+     .then(function(snap){
+         if(snap.empty) alert("Sin resultados") 
+         snap.forEach(doc=>{
+             resultsContainer.innerHTML = ""
+             let figure = document.createElement('figure')
+             figure.innerHTML = `
+                 <img src="${doc.data().link}" alt="${doc.data().title}" />
+             `
+             resultsContainer.appendChild(figure)
+         })
+     })
+}
 
 
 //FIREBASE LOGIN
 
-// function gmailLogin(){
-//     var provider = new firebase.auth.GoogleAuthProvider()
-//     firebase.auth()
-//         .signInWithPopup(provider)
-//         .then(function(result) {
-//             user = result.user
-//             setUser()
-//     })
-
-// }
+function gmailLogin(){
+	var provider = new firebase.auth.GoogleAuthProvider()
+	firebase.auth()
+		.signInWithPopup(provider)
+		.then(function(result) {
+			user = result.user
+			setUser()
+	})
+}
 
 
 
@@ -63,132 +63,131 @@ let user
 
 //USER LISTENER
 
-// firebase.auth().onAuthStateChanged(function(u){
-//     if(!u) console.log("Recuerda loguearte para publicar")
-//    else {
-//         user = u
-//         setUser()
-//     }
-// })
+firebase.auth().onAuthStateChanged(function(u){
+  if(!u) console.log("Recuerda loguearte para publicar")
+  else {
+    user = u
+    setUser()
+  }
+})
 
 //MEMES LISTENER
 
-// memesRef.onSnapshot(function(snap){
-//     snap.forEach(function(doc){
-//         drawResults([doc.data()])
-//     })
-// })
-//
+memesRef.onSnapshot(function(snap){
+  snap.forEach(function(doc){
+	drawResults([doc.data()])
+  })
+})
 
 // SEARCH FOR MEMES
 //
 
 //defaults listeners
 
-// searching.addEventListener('keydown', function(e){
-//     if(e.key == "Enter"){
-//         searchFor(e.target.value)
-//     }
-// })
+searching.addEventListener('keydown', function(e){
+  if(e.key == "Enter"){
+    searchFor(e.target.value)
+  }
+})
 
-// username.addEventListener('click', function(){
-//     if(!user){
-//         gmailLogin()
-//     }else{
-//         logout()
-//     }
-// })
+username.addEventListener('click', function(){
+  if(!user){
+	  gmailLogin()
+  }else{
+	  logout()
+  }
+})
 
 tache.addEventListener('click', function(){
-    clearModal()
+	clearModal()
 })
 
 //al dar click a subir meme
 subirButton.addEventListener('click', function(){
-    let meme = {
-        title: document.getElementById('title').value,
-        tags: document.getElementById('tags').value,
-        link:src
-    }
-    subirButton.disabled = true
-    subirButton.style = "background:grey"
-    //console.log(meme)
-    //UPLOAD TO FIREBASE
-    //writeMeme(meme)
-    //
+	let meme = {
+		title: document.getElementById('title').value,
+		tags: document.getElementById('tags').value,
+		link:src
+	}
+	subirButton.disabled = true
+	subirButton.style = "background:grey"
+	//console.log(meme)
+	//UPLOAD TO FIREBASE
+	writeMeme(meme)
+	//
 
-    //after upload
-    clearModal()
-    //FIREBASE
-    drawResults([meme])
-    
+	//after upload
+	clearModal()
+	//FIREBASE
+	//drawResults([meme])
+	
 })
 
 //al dar click a crear
 document.querySelector('.upload')
 .addEventListener('click', function(){
-    //FIREBASE
-    //if(!user) return alert("Inicia Sesión")
-    
-    fileInput.click()
+	//FIREBASE
+	//if(!user) return alert("Inicia Sesión")
+	
+	fileInput.click()
 })
 //al seleccionar la imagen
 fileInput.addEventListener('change', function(){
-    modal.style="display:flex"
-    let fr = new FileReader()
-    fr.readAsDataURL(fileInput.files[0])
-    fr.onload = function(){
-        src = fr.result
-        image.src = src
-    }
+	modal.style="display:flex"
+	let fr = new FileReader()
+	fr.readAsDataURL(fileInput.files[0])
+	fr.onload = function(){
+		src = fr.result
+		image.src = src
+	}
 })
 
 //aux functions
 function clearModal(){
-    fileInput.value = null
-    modal.style="display:none"
-    subirButton.disabled = false
-    subirButton.style = "linear-gradient(to right, purple, blueviolet)"
+	fileInput.value = null
+	modal.style="display:none"
+	subirButton.disabled = false
+	subirButton.style = "linear-gradient(to right, purple, blueviolet)"
 }
 
 function drawResults(array){
   // OPTIONAL
   //resultsContainer.innerHTML = ""
   //
-    array.forEach(function(el, index){
-        let figure = document.createElement('figure')
-        figure.innerHTML = `
-            <img src="${el.link}" alt="${index}">
-        `
-        resultsContainer.prepend(figure)
-    })
+	array.forEach(function(el, index){
+		let figure = document.createElement('figure')
+		figure.innerHTML = `
+			<img src="${el.link}" alt="${index}">
+		`
+		resultsContainer.prepend(figure)
+	})
 }
 
 function setUser(){
-    if(!user) {
-        document.querySelector('.user').innerHTML = `
-        <img id="photoURL" src="https://media.giphy.com/avatars/default3.gif" alt="">
-        <span id="username" >
-            Login
-        </span>
-        `
-        username = document.getElementById('username')
-        username.addEventListener('click', function(){
-            if(!user){
-                gmailLogin()
-            }else{
-                logout()
-            }
-        })
-        return
-    }
+	if(!user) {
+		document.querySelector('.user').innerHTML = `
+		<img id="photoURL" src="https://media.giphy.com/avatars/default3.gif" alt="">
+		<span id="username" >
+			Login
+		</span>
+		`
+		username = document.getElementById('username')
+		username.addEventListener('click', function(){
+			if(!user){
+				gmailLogin()
+			}else{
+				logout()
+			}
+		})
+		return
+	}
 
-    photoURL.src = user.photoURL
-    username.innerText = user.displayName
+	photoURL.src = user.photoURL
+	username.innerText = user.displayName
 }
 
 function logout(){
-    user = undefined
-    setUser()
-    //firebase.auth().signOut()
+	user = undefined
+	setUser()
+	//firebase.auth().signOut()
 }
